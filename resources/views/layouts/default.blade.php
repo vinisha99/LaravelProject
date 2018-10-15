d11ca85a3dd352af89fd90f036c756379ed513a1
vinisha <vinisha99@gmail.com>
2018-10-15 16:32:13 -0400

    Fix: added popper.js script in default.blade.js

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,14 +4,18 @@
     @include('includes.head')
 </head>
 <body>
+
 <header>
     @include('includes.header')
 </header>
+
 <main role="main">
+
     @yield('content')
     <footer class="row">
         @include('includes.footer')
     </footer>
+
 </main>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
