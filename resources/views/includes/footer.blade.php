5580895d13a421450e3bc61673f7fc2d3d732b37
vinisha <vinisha99@gmail.com>
2018-10-15 16:30:10 -0400

    Fix: Made alignment changes in the foote.blade.js

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,4 @@
-<div class="row mt-4 mb-4" class="font-weight-light" align="center">
-    &copy; 2017-2018 Learana, Inc. &middot; <a href="#"  class="font-weight-light">Privacy</a> &middot; <a href="#"  class="font-weight-light">Terms</a>
-</div>
+<footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2017-2018 Learana, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
