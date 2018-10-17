f0a9ed5be0ec2d91687d3d417aaec94ed07c8553
vinisha <vinisha99@gmail.com>
2018-10-16 22:41:21 -0400

    Task: Added contents in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h2>Welcome to edited home</h2>
+</br></br></br></br>
+    <h1 align="center">Welcome to Galaxies Home Page</h1>
+    <p align="center">
+        <img src="https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5OC81NDMvb3JpZ2luYWwvZ2FsYXh5LXNwaXJhbC5qcGc="
+         align="middle" style="width:800px;height:200px;" >
+    </p>
+    <p align="center"> Galaxies are sprawling space systems composed of dust, gas, and countless stars. </br>
+        The number of galaxies cannot be counted—the observable universe alone may contain 100 billion.</br>
+        Some of these distant systems are similar to our own Milky Way galaxy, while others are quite different.</br>
+        Galaxies with less than a billion stars are considered "small galaxies." In our own galaxy, the sun is just one of about 100 billion stars.
+    </p>
 
 @stop
