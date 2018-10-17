c9a8525e5b02a413c925277a485e4fbe780ce596
vinisha <vinisha99@gmail.com>
2018-10-16 22:47:30 -0400

    Task: Changed the title name

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="description" content="Learana Learning Analytics">
 <meta name="author" content="keith">
 
-<title>Learana - Learning Analytics</title>
+<title>Laravel - Galaxy Website</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
