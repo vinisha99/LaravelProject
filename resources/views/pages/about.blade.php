faa7d1fbff51b99b110e7e66a030c2f1d71025cb
vinisha <vinisha99@gmail.com>
2018-10-16 22:41:56 -0400

    Task: Added contents in the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.default')
 @section('content')
-    <h2>Welcome to edited about page</h2>
+
+        </br></br></br></br>
+
+        <h2 align="center">About Galaxies</h2>
+
+<table align="center">
+    <tr>
+        <td>
+            <img src="http://yaffa-cdn.s3.amazonaws.com/yaffadsp/images/dmImage/StandardImage/supermoonand%20neptune.jpg"
+                 style="width:300px;height:200px;">
+        </td>
+        <td>
+            <p>Galaxies are classified into three main types: spiral galaxies, elliptical galaxies, and irregular galaxies.</p>
+            <ul>
+                <li>Spiral galaxies, such as the Milky Way, consist of a flat disk with a bulging center and surrounding spiral arms. The galaxy's disk includes stars, planets, dust, and gas—all of which rotate around the galactic center in a regular manner. </li>
+                <li>Elliptical galaxies are shaped as their name suggests. They are generally round but stretch longer along one axis than along the other. They may be nearly circular or so elongated that they take on a cigarlike appearance.</li>
+                <li>Galaxies that are not spiral or elliptical are called irregular galaxies. Irregular galaxies appear misshapen and lack a distinct form, often because they are within the gravitational influence of other galaxies close by.</li>
+            </ul>
+
+        </td>
+    </tr>
+
+</table>
+</br></br></br></br>
 
 @stop
